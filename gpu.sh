457cb687f7a899796c9611f81fe2218b9d0448c6
Raymund Edgar <dev.raymund@gmail.com>
2025-04-04 21:43:48 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -9,3 +9,24 @@ curl -fsSL https://inference.supply/install.sh | sh
 curl -fsSL https://ollama.com/install.sh | sh
 curl -fsSL https://dria.co/launcher | bash
 export PATH="$HOME/.dria/bin:$PATH"
+
+# Edit the auto run when reboot
+echo "" >> /root/onstart.sh
+
+echo "# Kuzco" >> /root/onstart.sh
+echo "SESSION_NAME=\"kuzco\"" >> /root/onstart.sh
+echo "COMMAND=\"\"" >> /root/onstart.sh
+echo "tmux new-session -d -s $SESSION_NAME" >> /root/onstart.sh
+echo "tmux send-keys -t $SESSION_NAME \"$COMMAND\" C-m" >> /root/onstart.sh
+
+echo "# Gensyn" >> /root/onstart.sh
+echo "SESSION_NAME=\"gensyn\"" >> /root/onstart.sh
+echo "tmux new-session -d -s $SESSION_NAME" >> /root/onstart.sh
+echo "COMMAND=\"cd /root/rl-swarm && python3 -m venv .venv && source .venv/bin/activate\"" >> /root/onstart.sh
+echo "tmux send-keys -t $SESSION_NAME \"$COMMAND\" C-m" >> /root/onstart.sh
+
+echo "# Dria" >> /root/onstart.sh
+echo "SESSION_NAME=\"dria\"" >> /root/onstart.sh
+echo "tmux new-session -d -s $SESSION_NAME" >> /root/onstart.sh
+echo "COMMAND=\"dkn-compute-launcher start\"" >> /root/onstart.sh
+echo "tmux send-keys -t $SESSION_NAME \"$COMMAND\" C-m" >> /root/onstart.sh
