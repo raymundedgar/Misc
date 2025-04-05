839f1adf79c08d53835027339a5de6d060fbdafd
Raymund Edgar <dev.raymund@gmail.com>
2025-04-05 22:50:56 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -14,3 +14,10 @@ export PATH="$HOME/.dria/bin:$PATH"
 curl -sSL https://raw.githubusercontent.com/zunxbt/installation/main/node.sh | bash
 sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl screen git yarn && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt install -y yarn
 rm -rf rl-swarm && git clone https://github.com/SKaaalper/rl-swarm.git && cd rl-swarm
+
+cat << 'EOF' > "onstart.sh"
+#!/bin/bash
+tmux new-session -d -s "gensyn"
+tmux send-keys -t "gensyn" "cd /root/rl-swarm && python3 -m venv .venv && source .venv/bin/activate" C-m
+tmux attach -t "gensyn"
+EOF
