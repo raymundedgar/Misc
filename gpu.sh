e7ef8533e3cde7cfc1495ada8a7946186de07ec1
Raymund Edgar <dev.raymund@gmail.com>
2025-04-08 01:15:00 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -5,19 +5,4 @@ apt update && apt install -y sudo curl tmux pciutils lshw
 # Kuzco
 curl -fsSL https://inference.supply/install.sh | sh
 
-# Dria
-curl -fsSL https://ollama.com/install.sh | sh
-curl -fsSL https://dria.co/launcher | bash
-export PATH="$HOME/.dria/bin:$PATH"
-
-# Gensyn
-curl -sSL https://raw.githubusercontent.com/zunxbt/installation/main/node.sh | bash
-sudo apt update && sudo apt install -y python3 python3-venv python3-pip curl screen git yarn && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && sudo apt install -y yarn
-rm -rf rl-swarm && git clone https://github.com/SKaaalper/rl-swarm.git && cd rl-swarm
-
-cat << 'EOF' > "/root/onstart.sh"
-#!/bin/bash
-tmux new-session -d -s "gensyn"
-tmux send-keys -t "gensyn" "cd /root/rl-swarm && python3 -m venv .venv && source .venv/bin/activate" C-m
-tmux attach -t "gensyn"
-EOF
+kuzco worker start --worker CL7LPiShlb-h0CTiFD45w --code 48b615d9-19f1-42f2-85f8-a6d4d642d988
