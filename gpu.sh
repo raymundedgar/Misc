a1168862c2a5e133ba31fe4bdb52bff68b771ec7
Raymund Edgar <dev.raymund@gmail.com>
2025-04-08 08:08:29 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,7 +4,3 @@ apt update && apt install -y sudo curl tmux
 
 # Kuzco
 curl -fsSL https://inference.supply/install.sh | sh
-
-tmux new-session -d -s kuzco
-tmux send-keys -t kuzco 'kuzco worker start --worker CL7LPiShlb-h0CTiFD45w --code 48b615d9-19f1-42f2-85f8-a6d4d642d988' C-m
-tmux attach-session -t kuzco
