567455919cf73e6a706927d4a737eaf2145a0d4d
Raymund Edgar <dev.raymund@gmail.com>
2025-04-16 23:43:25 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,3 +4,8 @@ apt update && apt install -y sudo curl tmux pciutils
 
 # Kuzco
 curl -fsSL https://inference.supply/install.sh | sh
+
+# Dria
+curl -fsSL https://ollama.com/install.sh | sh
+curl -fsSL https://dria.co/launcher | bash
+export PATH="$HOME/.dria/bin:$PATH"
