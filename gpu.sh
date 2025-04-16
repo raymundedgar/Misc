a056d88c89ce50038e0bc4c5009cd72ae643ecf4
Raymund Edgar <dev.raymund@gmail.com>
2025-04-16 23:57:07 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -11,4 +11,7 @@ curl -fsSL https://inference.supply/install.sh | sh
 # Dria
 curl -fsSL https://dria.co/launcher | bash
 export PATH="$HOME/.dria/bin:$PATH"
+cd ~/.dria && mkdir -p dkn-compute-launcher
+tmux new-session -d -s dria
+
 curl -fsSL https://ollama.com/install.sh | sh
