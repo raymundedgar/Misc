c572a9475cbdf59705f196b0650cc843b45ad4e1
Raymund Edgar <dev.raymund@gmail.com>
2025-04-18 17:44:52 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,11 +2,10 @@
 
 apt update && apt install -y sudo curl tmux pciutils
 
-# open tmux session
-tmux
-
 # Kuzco
 curl -fsSL https://inference.supply/install.sh | sh
+tmux new-session -d -s "kuzco"
+tmux send-keys -t "kuzco" "kuzco worker start --worker FxAJL3XlkTTrh5-MwkO-x --code 27b8451a-80f9-4991-af98-33d27d4a3fb4" C-m
 
 # Dria
 curl -fsSL https://dria.co/launcher | bash
