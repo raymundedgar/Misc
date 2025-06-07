325dd7f0d5af211a51184609e4b95493d8a9a765
Raymund Edgar <dev.raymund@gmail.com>
2025-06-07 11:18:21 +0800

    Update kuzco.sh

--- kuzco.sh
+++ kuzco.sh
@@ -3,7 +3,7 @@
 apt update && apt install -y sudo curl tmux pciutils
 
 # Kuzco
-curl -fsSL https://inference.supply/install.sh | sh
+curl -fsSL https://devnet.inference.net/install.sh | sh
 tmux new-session -d -s "kuzco"
-tmux send-keys -t "kuzco" "kuzco worker start --worker FxAJL3XlkTTrh5-MwkO-x --code 27b8451a-80f9-4991-af98-33d27d4a3fb4" C-m
+tmux send-keys -t "inference node start --code 4be10240-1519-43dd-879b-818744aa2a06" C-m
 tmux attach -t kuzco
